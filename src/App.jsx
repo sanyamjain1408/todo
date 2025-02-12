854a909e30359e7e851fc5463a968d721d85841b
Sanyam Jain <156403166+sanyamkanu@users.noreply.github.com>
2025-02-12 13:23:36 +0530

    todoapp

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-black bg-pink-500'>hello ji kese hai aap sab log</h1>
+    </>
+  )
+}
+
+export default App
